03e2e5772edd1b9de8a31d1f24da3ea2c8c997cc
Den <52295780+josserden@users.noreply.github.com>
2021-08-11 21:35:13 +0300

    upd

---
+++ precache-manifest.5214dba38fa200e6391e692b51f82781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e2a51794ba8935a044d42c721986bf",
+    "url": "/Tutorials/Mirrors/index.html"
+  },
+  {
+    "revision": "92ecda87abe1a59642ea",
+    "url": "/Tutorials/Mirrors/static/css/main.8d7da27f.chunk.css"
+  },
+  {
+    "revision": "4f14d17b7098a71d1040",
+    "url": "/Tutorials/Mirrors/static/js/2.012d5caa.chunk.js"
+  },
+  {
+    "revision": "4a39ab3be31c11d2dea3d04487e78b26",
+    "url": "/Tutorials/Mirrors/static/js/2.012d5caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ecda87abe1a59642ea",
+    "url": "/Tutorials/Mirrors/static/js/main.f3027470.chunk.js"
+  },
+  {
+    "revision": "7c954e1fe14189945e1a",
+    "url": "/Tutorials/Mirrors/static/js/runtime-main.3f5545e1.js"
+  }
+]);
